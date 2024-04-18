5bd9e56fbc7e3a2c5ad2dcc898125706c2de8861
gbbrambila <gb.brambila@gmail.com>
2024-04-18 16:29:30 -0300

    Implemented client and server actions

---
+++ src/app/actions/service.ts
@@ -0,0 +1,23 @@
+import { LogFileListType, LogFileLoaded } from "@/lib";
+
+//TODO: move to an env var to get it read to prod
+const API_BASE_URL = `http://localhost:3000`;
+
+export async function listLogFiles(): Promise<{ data: LogFileListType[] }> {
+  const res = await fetch(`${API_BASE_URL}/api/log/list`, {
+    cache: "no-store",
+  });
+  return res.json();
+}
+
+export async function loadLogFile(
+  logFilePath: string
+): Promise<{ data: LogFileLoaded; status: number; error: string }> {
+  const res = await fetch(`${API_BASE_URL}/api/log/load/?path=${logFilePath}`, {
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  return { ...data, status: res.status };
+}
